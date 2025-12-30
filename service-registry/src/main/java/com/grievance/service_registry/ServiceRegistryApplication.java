01c04dce1e5f3b50985d795f13dcba3d5a6cd9d1
Debarghya Sikder <debarghya1412@gmail.com>
2025-12-30 22:03:24 +0530

    Successfully started the auth service

--- service-registry/src/main/java/com/grievance/service_registry/ServiceRegistryApplication.java
+++ service-registry/src/main/java/com/grievance/service_registry/ServiceRegistryApplication.java
@@ -2,12 +2,14 @@ package com.grievance.service_registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceRegistryApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegistryApplication.class, args);
+    }
 }
