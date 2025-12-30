fa862d67793d0df7e2192b48bc8d57c06907245b
Debarghya Sikder <debarghya1412@gmail.com>
2025-12-30 23:57:17 +0530

    Created the Auth Controller of the auth service

---
+++ auth-service/src/main/java/com/grievance/auth_service/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.grievance.auth_service.controller;
+
+import com.grievance.auth_service.dto.AuthResponse;
+import com.grievance.auth_service.dto.LoginRequest;
+import com.grievance.auth_service.dto.RegisterRequest;
+import com.grievance.auth_service.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        AuthResponse response = authService.register(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        AuthResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
+}
