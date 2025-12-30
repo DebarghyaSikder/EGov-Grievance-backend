8c0dafdb4304582bc3e520c6d9e5ce4f02af27d8
Debarghya Sikder <debarghya1412@gmail.com>
2025-12-30 12:34:02 +0530

    Corrected the configuration files

---
+++ config-server/src/test/java/com/grievance/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grievance.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
