91e6749e6dba0e7e0a1fc9103001b9d5c2aaacbc
Debarghya Sikder <debarghya1412@gmail.com>
2025-12-31 00:53:48 +0530

    Added Bean validation to RegisterRequest

--- auth-service/src/main/java/com/grievance/auth_service/dto/RegisterRequest.java
+++ auth-service/src/main/java/com/grievance/auth_service/dto/RegisterRequest.java
@@ -1,18 +1,31 @@
 package com.grievance.auth_service.dto;
 
-import com.grievance.auth_service.entity.Role;
+import jakarta.validation.constraints.*;
+
 import lombok.*;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
+
+    @NotBlank(message = "Full name is required")
     private String fullName;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
     private String email;
+
+    @Pattern(regexp = "^[0-9]{12}$", message = "Aadhaar number must be 12 digits")
+    @NotBlank(message = "Aadhaar number is required")
     private String aadhaarNumber;
+
+    @Size(min = 8, max = 20, message = "Password must be between 8–20 characters")
+    @NotBlank(message = "Password is required")
     private String password;
+
+    @Pattern(regexp = "^[0-9]{10}$", message = "Phone must be a 10-digit number")
+    @NotBlank(message = "Phone is required")
     private String phone;
-    private Role role;   // will default to CITIZEN in controller for self-registration
 }
