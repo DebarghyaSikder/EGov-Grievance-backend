170971cee5bde0ff01812bdd50b7bee53ac729b4
Debarghya Sikder <debarghya1412@gmail.com>
2025-12-31 21:30:14 +0530

    Added the enums for grievance service

---
+++ grievance-service/src/main/java/com/grievance/grievance_service/enums/Priority.java
@@ -0,0 +1,8 @@
+package com.grievance.grievance_service.enums;
+
+public enum Priority {
+    LOW,
+    MEDIUM,
+    HIGH,
+    CRITICAL
+}
---
+++ grievance-service/src/main/java/com/grievance/grievance_service/enums/Status.java
@@ -0,0 +1,8 @@
+package com.grievance.grievance_service.enums;
+
+public enum Status {
+    PENDING,
+    IN_PROGRESS,
+    RESOLVED,
+    REJECTED
+}
