084ed25f83879eebb2d32e45eb9f928b65601ce3
Debarghya Sikder <debarghya1412@gmail.com>
2025-12-31 22:18:32 +0530

    repository for grievance history done

---
+++ grievance-service/src/main/java/com/grievance/grievance_service/repository/GrievanceHistoryRepository.java
@@ -0,0 +1,5 @@
+package com.grievance.grievance_service.repository;
+
+public class GrievanceHistoryRepository {
+
+}
---
+++ grievance-service/src/main/java/com/grievance/grievance_service/repository/GrievanceRepository.java
@@ -0,0 +1,21 @@
+package com.grievance.grievance_service.repository;
+
+import com.grievance.grievance_service.entity.Grievance;
+import com.grievance.grievance_service.enums.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface GrievanceRepository extends JpaRepository<Grievance, Long> {
+
+    Optional<Grievance> findByGrievanceNumber(String grievanceNumber);
+
+    List<Grievance> findByCitizenId(Long citizenId);
+
+    List<Grievance> findByAssignedOfficerId(Long officerId);
+
+    List<Grievance> findByStatus(Status status);
+}
