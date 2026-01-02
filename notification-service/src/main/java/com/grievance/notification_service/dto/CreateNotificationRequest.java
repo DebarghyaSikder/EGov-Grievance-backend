29869424872e4d17b1ac44d17eb18af8e68ac6fb
Debarghya Sikder <debarghya1412@gmail.com>
2026-01-02 13:39:17 +0530

    Notification request created in dto

---
+++ notification-service/src/main/java/com/grievance/notification_service/dto/CreateNotificationRequest.java
@@ -0,0 +1,34 @@
+package com.grievance.notification_service.dto;
+
+import com.grievance.notification_service.enums.NotificationType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateNotificationRequest {
+
+    @NotNull(message = "User ID is required")
+    private Long userId;
+
+    private Long grievanceId;
+
+    private String grievanceNumber;
+
+    @NotNull(message = "Notification type is required")
+    private NotificationType type;
+
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    @NotBlank(message = "Message is required")
+    private String message;
+
+    private String userEmail;
+}
