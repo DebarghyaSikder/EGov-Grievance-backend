5c8ace83fee510d4b31d7e61fbfea9d66072b471
Debarghya Sikder <debarghya1412@gmail.com>
2026-01-02 13:32:45 +0530

    Added the enums for the notification types

---
+++ notification-service/src/main/java/com/grievance/notification_service/enums/NotificationType.java
@@ -0,0 +1,10 @@
+package com.grievance.notification_service.enums;
+
+public enum NotificationType {
+    GRIEVANCE_SUBMITTED,
+    GRIEVANCE_ASSIGNED,
+    STATUS_CHANGED,
+    GRIEVANCE_RESOLVED,
+    GRIEVANCE_CLOSED,
+    FEEDBACK_RECEIVED
+}
