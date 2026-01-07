c5c5587deaadbd211ee00e13c0c61aae0c7446cd
Debarghya Sikder <debarghya1412@gmail.com>
2026-01-07 15:13:54 +0530

    File modified

--- notification-service/src/main/java/com/grievance/notification_service/config/SecurityConfig.java
+++ notification-service/src/main/java/com/grievance/notification_service/config/SecurityConfig.java
@@ -17,8 +17,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()
-            );
+                .anyRequest().permitAll());
 
         return http.build();
     }
